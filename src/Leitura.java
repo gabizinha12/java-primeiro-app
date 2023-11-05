e39c16702272ce821f77a50d4b1acb068923cfda
gabizinha12 <gaibou_lima@hotmail.com>
2023-11-05 00:46:09 -0300

    Nova classe para leitura de dados

--- src/Leitura.java
+++ src/Leitura.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Leitura {
     public static void main(String[] args) {
-
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite seu filme favorito!");
+        String filme = leitura.nextLine();
+        System.out.println(filme);
+        System.out.println("Qual o ano de lançamento?");
+        int anoDeLancamento = leitura.nextInt();
+        System.out.println(anoDeLancamento);
     }
 }
