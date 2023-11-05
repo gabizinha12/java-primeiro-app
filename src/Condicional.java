8a034f168ada97acd480f9ce7ec32e240f9a6d9f
gabizinha12 <gaibou_lima@hotmail.com>
2023-11-05 00:06:07 -0300

    Condicionais e Leitura de dados

--- src/Condicional.java
+++ src/Condicional.java
@@ -1,7 +1,18 @@
 public class Condicional {
     public static void main(String[] args) {
         int anoDeLancamento = 2022;
-        boolean incluidoNoPlano = true;
+        boolean incluidoNoPlano = false;
         double notaDoFilme = 8.1;
+        String tipoPlano = "normal";
+        if(anoDeLancamento >= 2022) {
+            System.out.println("Lançamentos que os clientes estão curtindo!");
+        } else {
+            System.out.println("Filme retrô que vale a pena assistir!");
+        }
+        if(incluidoNoPlano == true && tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar a locação");
+        }
     }
 }
